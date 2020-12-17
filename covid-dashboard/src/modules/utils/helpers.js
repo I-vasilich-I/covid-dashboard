4b2a3da48f71ff9d19aa6e15aababa49af22ef84
Oleg Vaskevich <oleg_vas08@mail.ru>
2020-12-17 11:27:38 +0300

    init: start work on table

---
+++ covid-dashboard/src/modules/utils/helpers.js
@@ -0,0 +1,58 @@
+/**
+ *  Returns array sorted by property in given order (1/-1).
+ *  @param {Array} array
+ *  @param {String} property
+ *  @param {Number} order
+ *  @return {Array}
+ */
+function sortByProperty(array, property, order = 1) {
+  array.sort((element1, element2) => {
+    if (element1[property] < element2[property]) return -1 * order;
+    if (element1[property] > element2[property]) return 1 * order;
+    return 0;
+  });
+  return array;
+}
+/**
+ *
+ * @param {Object} obj
+ * @returns {Element}
+ * obj = {string, string, DOMElement, [attributes]}
+ */
+function createDomElement(obj) {
+  let element = null;
+  const { elementName, className = null, parent = null, attributes = [] } = obj;
+  if (!elementName) return null;
+
+  try {
+    element = document.createElement(elementName);
+  } catch (e) {
+    throw new Error('Unable to create element. Wrong element name');
+  }
+
+  if (className) {
+    element.classList.add(...className.split(' '));
+  }
+
+  if (parent) {
+    parent.appendChild(element);
+  }
+
+  if (attributes.length) {
+    const regEx = /value|id|placeholder|rows|autocorrect|spellcheck|src|alt/;
+    attributes.forEach(([attrName, attrValue]) => {
+      if (!attrValue) {
+        element.setAttribute(attrName, '');
+      }
+      const isMatch = attrName.match(regEx);
+      if (isMatch) {
+        element.setAttribute(attrName, attrValue);
+      } else {
+        element.dataset[attrName] = attrValue;
+      }
+    });
+  }
+  return element;
+}
+
+export { createDomElement, sortByProperty };
